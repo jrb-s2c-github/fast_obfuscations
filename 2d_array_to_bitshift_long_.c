a7c21c82edd53cc22a92bf796c71f67f079dcea7
Jan-Rudolph Bührmann @ S2C Consulting <89936232+jrb-s2c-github@users.noreply.github.com>
2026-08-27 22:25:57 +0200

    Create 2d_array_to_bitshift_long_.c

---
+++ 2d_array_to_bitshift_long_.c
@@ -0,0 +1,14 @@
+/*
+ *
+ * Turn content of two-dimensional array into long by adding bitshifted values together, e.g.
+ *   [ 2 0 1 ]
+ *   [ 2 1 0 ]  ==> 001 100 010 001 010 100 010 001 100
+ *   [ 1 2 0 ]
+ *
+ */
+long new_state = 0;
+for (int i = 0; i < dimension; i++) {
+  for (int j = 0; j < dimension; j++) {
+    new_state += 1 << grid[state_index][i][j]<<dimension*(i*dimension+j);
+  }
+}
